e7ba84edaf3b300ad1a873c5e484a62ff7351e1b
aska <seki.mist@gmail.com>
2016-06-21 21:22:07 +0900

    6/21

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class Controller {
-}
+
+    @FXML private Button helloButton;
+    @FXML private Label helloLabel;
+
+    private int cnt;
+
+    @FXML
+    public void onHelloButtonClicked(ActionEvent event) {
+        this.helloLabel.setText("clicked! : " + cnt);
+        cnt++;
+    }
+
+
+    }
